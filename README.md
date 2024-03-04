7ed9882bf1b574270041a23ae9496b4923d74f0f
quicherat <enriquevillasolis@gmail.com>
2024-03-03 19:37:45 -0500

    Agregamos desarrollo hasta antes de la conexion con la DB

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Proyecto empezado el 3/03/2024
 para inicializar el servidor se usa *npm run dev*
+
+Se ha instalado morgan con *npm i morgan*
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import app from './app.js';
+import {dbConnection} from './db.js';
+
+// dbConnection();
+app.listen(4000)
+console.log('server on port', 4000);
